3b9639bfa863fdb086de1704582a3d8553c23ca7
HHetmanus <111887858+HHetmanus@users.noreply.github.com>
2023-01-02 09:45:10 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const trzy = document.getelementByID("trzy");
 const cztery = document.getelementByID("cztery");
 const piec = document.getelementByID("piec");
 
+
+dwa.style.display= "none";
+trzy.style.display= "none";
+cztery.style.display= "none";
+piec.style.display= "none";
