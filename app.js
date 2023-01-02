c826788b138c58fe772920f2f69846e826e26ef6
HHetmanus <111887858+HHetmanus@users.noreply.github.com>
2023-01-02 09:41:19 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+const jeden = document.getelementByID("jeden");
+const dwa = document.getelementByID("dwa");
+const trzy = document.getelementByID("trzy");
+const cztery = document.getelementByID("cztery");
+const piec = document.getelementByID("piec");
 
