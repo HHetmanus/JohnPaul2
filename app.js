81ef18dbc04fa14bd8f143e35e86811a136ef015
HHetmanus <111887858+HHetmanus@users.noreply.github.com>
2023-01-16 13:14:45 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ let tekstTrzy = document.getElementById("trzy");
 let tekstCztery = document.getElementById("cztery");
 let tekstPiec = document.getElementById("piec");
 
-let forwardOne = document.getElementById("for1");
+let forwardOne = document.getElementByClassName("fas fa-backward fa-xl");
 let forwardTwo = document.getElementById("for2");
 let forwardThree = document.getElementById("for3");
 let forwardFour = document.getElementById("for4");
 let forwardFive = document.getElementById("for5");
 
-let backwardOne = document.getElementById("back1");
+let backwardOne = document.getElementByID("back1");
 let backwardTwo = document.getElementById("back2");
 let backwardThree = document.getElementById("back3");
 let backwardFour = document.getElementById("back4");
