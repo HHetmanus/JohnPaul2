93201002dca8c455a4158992067d141088363bdc
HHetmanus <111887858+HHetmanus@users.noreply.github.com>
2023-01-16 13:29:00 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ let tekstTrzy = document.getElementById("trzy");
 let tekstCztery = document.getElementById("cztery");
 let tekstPiec = document.getElementById("piec");
 
-let forwardOne = document.querySelector("#for1");
+//let forwardOne = document.querySelector("#for1");
 //let forwardTwo = document.getElementById("for2");
 //let forwardThree = document.getElementById("for3");
 //let forwardFour = document.getElementById("for4");
 //let forwardFive = document.getElementById("for5");
 
-let backwardOne = document.getElementByID("back1");
+//let backwardOne = document.getElementByID("back1");
 //let backwardTwo = document.getElementById("back2");
 //let backwardThree = document.getElementById("back3");
 //let backwardFour = document.getElementById("back4");
@@ -25,7 +25,7 @@ tekstTrzy.hidden = true;
 tekstCztery.hidden = true;
 tekstPiec.hidden = true;
 
-forwardOne.hidden = true;
+//forwardOne.hidden = true;
 /*forwardTwo.hidden = true;
 forwardThree.hidden = true;
 forwardFour.hidden = true;
